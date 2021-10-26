e1e0fc8708936fde52ba8a6bfe013170dacc381d
Njeri Muchiri <muchirinjeri3210@gmail.com>
2021-10-26 17:15:27 +0300

    added some styling on my css page and created new divs on my container component

--- src/components/container.js
+++ src/components/container.js
@@ -1,10 +1,24 @@
 import React from "react";
-
+import boss from '../images/headerimg.jpg';
+import '../App.css';
 
 function Container(){
       return(
           <>
-          
+          <section className="me-section">
+              <article className="about-me">
+            <div className="introduction"><h1>Hi I'm Ann </h1>
+             <p>Freelancer in Website Development ||
+             Mobile Design, UX/UI.
+              </p>
+              <a href="/contact" className="hero-btn">Hireme</a>
+             </div>
+             <div className="photo-section">
+                <img src={boss} alt="NjeriMuchiri" className="me-image" />
+             </div>
+              </article>
+
+          </section>
       
           </>
       )
