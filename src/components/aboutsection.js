087b7c4aa8d6aea48d1a6a1a0d53bc265e053f0a
Njeri Muchiri <muchirinjeri3210@gmail.com>
2021-10-26 18:33:29 +0300

    added styling on my app styling css and added a paragraph element on my about scetion

--- src/components/aboutsection.js
+++ src/components/aboutsection.js
@@ -11,6 +11,11 @@ function AboutSection(){
                <article className="photo-me">
                     <img src={girlboss} alt="NjeriMuchiri" className="me-image" />
                </article>
+               <p className="aboutme-home">
+                Being a website designer and developer is currently
+                and has been my second nature for the last 1 year and a half that I have been in this field.As I embark on this journey of designing,I look forward to coming up
+                with great products,that will make my clients happy and contented.
+               </p>
             </div>
         </section>
         </>
