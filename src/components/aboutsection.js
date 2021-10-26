2e602b1783e740b0e9ccf194f6f35cd9a0ff6ece
Njeri Muchiri <muchirinjeri3210@gmail.com>
2021-10-26 20:18:42 +0300

    Added new elements on our about section page and added some styling on our app css styling page

--- src/components/aboutsection.js
+++ src/components/aboutsection.js
@@ -9,13 +9,11 @@ function AboutSection(){
         <section className="part">
             <div className="isall-about">
                <article className="photo-me">
+                   <div className="info-title">
+                      <h2>About</h2>
+                      </div>
                     <img src={girlboss} alt="NjeriMuchiri" className="me-image" />
                </article>
-               <p className="aboutme-home">
-                Being a website designer and developer is currently
-                and has been my second nature for the last 1 year and a half that I have been in this field.As I embark on this journey of designing,I look forward to coming up
-                with great products,that will make my clients happy and contented.
-               </p>
             </div>
         </section>
         </>
