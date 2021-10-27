e56d291d42033816fe717d22799289611cdeaac7
Njeri Muchiri <muchirinjeri3210@gmail.com>
2021-10-27 10:59:40 +0300

    created new elements and got rid of the previous elements i had earlier created

--- src/components/aboutsection.js
+++ src/components/aboutsection.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import '../App.css';
 import girlboss from '../images/aboutme.jpg';
+import boss from '../images/headerimg.jpg';
+
 
 
 function AboutSection(){
     return(
         <>
-        <section className="part">
-            <div className="isall-about">
-               <article className="photo-me">
-                   <div className="info-title">
-                      <h2>About</h2>
-                      </div>
-                    <img src={girlboss} alt="NjeriMuchiri" className="me-image" />
-               </article>
+        <section class="part">
+      <div class="section-title">
+        <h2>About</h2>
+      </div>
+      <div class="section-center blog-center">
+        <div class="card">
+          <div class="card-side card-front">
+            <img src={girlboss} alt="NjeriMuchiri" />
+            <div class="card-info">
+              <h4>About Me</h4>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et
+                nisi ut a est eum tempora dolorum temporibus voluptatibus!
+                Natus, provident.
+              </p>
+              <div class="card-footer">
+                <img src={boss} alt="Njerimuchiri" />
+                <button class="btn">read more</button>
+          </div>
             </div>
+        </div>
+        </div>
+        </div>
         </section>
         </>
-    )
-}
+    );
+};
 
 export default AboutSection;
