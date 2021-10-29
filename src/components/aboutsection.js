211399183f283240945f833e7c975eb801b207a9
Njeri Muchiri <muchirinjeri3210@gmail.com>
2021-10-29 13:19:41 +0300

    Deleted all the content in my component page and starting all over from the app styling page too

--- src/components/aboutsection.js
+++ src/components/aboutsection.js
@@ -1,37 +1,14 @@
 import React from 'react';
 import '../App.css';
-import girlboss from '../images/aboutme.jpg';
-import boss from '../images/headerimg.jpg';
+// import girlboss from '../images/aboutme.jpg';
+// import boss from '../images/headerimg.jpg';
 
 
 
 function AboutSection(){
     return(
         <>
-        <section class="part">
-      <div class="section-title">
-        <h2>About</h2>
-      </div>
-      <div class="section-center blog-center">
-        <div class="card">
-          <div class="card-side card-front">
-            <img src={girlboss} alt="NjeriMuchiri" />
-            <div class="card-info">
-              <h4>About Me</h4>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et
-                nisi ut a est eum tempora dolorum temporibus voluptatibus!
-                Natus, provident.
-              </p>
-              <div class="card-footer">
-                <img src={boss} alt="Njerimuchiri" />
-                <button class="btn">read more</button>
-          </div>
-            </div>
-        </div>
-        </div>
-        </div>
-        </section>
+       
         </>
     );
 };
