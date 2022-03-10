a56bb1cd731d5ce0ea4ac327c538cb984efbdb3e
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-10 17:56:01 +0300

    Reinitial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
-import Home from './pages/home';
-import Contact from "./pages/contact";
-import About from "./pages/about";
-import Project from "./pages/projects";
-import Navigation from "./components/nav";
+
+
 
 
 function App(){
 return(
- <Router>
-   <Navigation/>
-   <Switch>
-     <Route path="/" exact component={Home} />
-     <Route path="/About"   component={About} />
-     <Route path="/Projects"  component={Project} />
-     <Route path="/Contact"  component={Contact} />
-     
-   </Switch>
-  </Router>
-);
+  <div>
+    <h1>Kasheee!!!</h1>
+  </div>
+)
+  
 }
 
 
