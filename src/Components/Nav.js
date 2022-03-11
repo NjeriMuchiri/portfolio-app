8eb1ecae02fc54f804299a03340ad7ffb0c32bcb
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-11 10:04:04 +0300

    Creating the navigation feature

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './nav.css';
 import '../App.css';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAlignLeft } from '@fortawesome/free-solid-svg-icons';
+import Logo from '../images/logo1.png';
 
 const Nav = () => {
   return (
-    <section className='all-section'>
+    <>
     <nav className='nav-sect'>
+      <div className='nav-header'>
+      <Link to='/'><img className='logo' src={Logo} alt="logo" /></Link> 
+       <button className='nav-toggle'>
+         <FontAwesomeIcon icon={faAlignLeft} />
+       </button>
+      </div>
       <ul className='nav-list'>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/Skills">Skills</Link></li>
@@ -17,7 +26,7 @@ const Nav = () => {
       </ul>
 
     </nav>
-    </section>
+    </>
   )
 }
 
