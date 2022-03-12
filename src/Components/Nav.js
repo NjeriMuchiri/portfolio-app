ffae87bd881dd59b772e26e7013c192f8b700a92
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-12 13:36:06 +0300

    Finished on the navigation, on to the next step now

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,7 +3,7 @@ import './nav.css';
 import '../App.css';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAlignLeft} from '@fortawesome/free-solid-svg-icons';
+import { faAlignLeft,faChessQueen} from '@fortawesome/free-solid-svg-icons';
 import Logo from '../images/logo1.png';
 
 const Nav = () => {
@@ -19,7 +19,8 @@ const Nav = () => {
       <div className='nav-header'>
       <Link to='/'><img className='logo' src={Logo} alt="logo" /></Link> 
        <button onClick={handleClick} className='nav-toggle'>
-         <FontAwesomeIcon icon={faAlignLeft} />
+         {toggle ? <FontAwesomeIcon icon={faChessQueen}/> :
+         <FontAwesomeIcon icon={faAlignLeft}/>}
        </button>
       </div>
       <ul className={toggle ? 'show-nav-list' : 'nav-list'}>
