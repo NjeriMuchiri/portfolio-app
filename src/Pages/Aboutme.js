aa9ed5d3ee890d0bf331f4671d8fe3c50277e4da
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-14 13:18:12 +0300

    Creating my about page as I think of my homepage design

--- src/Pages/Aboutme.js
+++ src/Pages/Aboutme.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Kashee from '../images/aboutme.jpg';
 
 const Aboutme = () => {
   return (
-    <div>Aboutme</div>
+    <div className='aboutme-section'>
+       <article className='info-section'>
+       <img src={Kashee} alt="NjeriMuchiri" />
+       <p className='kashee-info'>
+         Hi,my name is NjeriMuchiri and I am a webdeveloper,webdesigner and an SEO practitioner. I have been in this creative space for a while now and I am well familiar and experienced with everything that goes around in the process of developing great products that will have best user experience and one that will be ranked high by the Search Engines.
+       </p>
+       </article>
+    </div>
   )
 }
 
