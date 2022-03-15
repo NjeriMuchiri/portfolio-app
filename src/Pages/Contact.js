e94a8bf5917e4471dccad516cb332421fca35882
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-15 19:22:14 +0300

    Creating my contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,8 +1,24 @@
-import React from 'react'
-
+import React from 'react';
+import '../Components/Contact.css';
+import Footer from '../Components/Footer';
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    <div className='contact-banner'>
+      <article className='contact-info'>
+         <form className='message-form'>
+           <p className='info-cont'>Leave a Message</p>
+               <input type="text" name="name" placeholder='Name'/>
+               <input type="email" name="email" placeholder='Your Email' />
+               <input type="textarea" name="message" placeholder='Message'/>
+             <input type="submit" value="submit" />
+         </form>
+
+      </article>
+    </div>
+    <Footer/>
+      
+    </>
   )
 }
 
