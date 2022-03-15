8f67aaa90d354ac867bee05c1ad8003411c24405
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-15 13:30:08 +0300

    Adding some styling to avoid an overflow

--- src/Pages/Aboutme.js
+++ src/Pages/Aboutme.js
@@ -12,7 +12,7 @@ const Aboutme = () => {
        src={Kashee}  
        alt="NjeriMuchiri"  />
        <p className='kashee-info'>
-         Hi,my name is NjeriMuchiri and I am a <span>WebsiteDeveloper, WebsiteDesigner and an SEO practitioner</span>. I have been in this creative space for a while now and I am well familiar and experienced with everything that goes around in the process of developing great products that will have best user experience and one that will be ranked high by the Search Engines. I look forward to working with and for anyone who would like to work with me. Check out my contact page for the contact info and let's connect. See you soon <FontAwesomeIcon icon={faSmileBeam} style={{color:'#f5c71a'}} />.
+         Hi,my name is NjeriMuchiri and I am a <span>WebsiteDeveloper, WebsiteDesigner and an SEO practitioner</span>. I have been in this creative space for a while now and I am well familiar and experienced with everything that goes around in the process of developing great products that will have best user experience and one that will be ranked high by the Search Engines. I look forward to working with and for anyone who would like to work with me. Check out my contact page for the contact info and let's connect. See you soon <FontAwesomeIcon icon={faSmileBeam} style={{color:'#f5c71a'}} />
        </p>
        </article>
     </div>
