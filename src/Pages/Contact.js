a7c61e3e1c8d1d00f581df5217a1df523b0d97a8
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-16 10:40:32 +0300

    Designing my contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,3 +1,5 @@
+import { faCommentDots } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import '../Components/Contact.css';
 import Footer from '../Components/Footer';
@@ -7,7 +9,7 @@ const Contact = () => {
     <div className='contact-banner'>
       <article className='contact-info'>
          <form className='message-form'>
-           <p className='info-cont'>Leave a Message</p>
+           <p className='info-cont'>Leave a Message<FontAwesomeIcon icon={faCommentDots}style={{color:'#9bddff',marginLeft:5,fontSize:20}}/></p>
                <input type="text" name="name" placeholder='Name'/>
                <input type="email" name="email" placeholder='Your Email' />
                <input type="textarea" name="message" placeholder='Message'/>
