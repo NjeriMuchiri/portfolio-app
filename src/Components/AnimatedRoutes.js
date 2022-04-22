2cbec2b4fcf1e18ddd86a1284dfa2c3066980148
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-04-22 10:31:47 +0300

    Adding animation transitions

--- src/Components/AnimatedRoutes.js
+++ src/Components/AnimatedRoutes.js
@@ -8,16 +8,19 @@ import Home from '../Pages/Home';
 import Skills from '../Pages/Skills';
 import Aboutme from '../Pages/Aboutme';
 import Contact from '../Pages/Contact';
+import {AnimatePresence} from 'framer-motion/dist/framer-motion';
 
 function AnimatedRoutes(){
     const location = useLocation();
   return (
-      <Switch>
+      <AnimatePresence>
+      <Switch locations = {location} key = {location.pathname}>
       <Route path="/" exact component={Home} />
       <Route path="/Skills" exact component={Skills} />
       <Route path="/Aboutme" exact component={Aboutme} />
       <Route path="/Contact" exact component={Contact} />
   </Switch>
+  </AnimatePresence>
   )
 }
 export default AnimatedRoutes
