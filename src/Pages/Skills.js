f2a9722dae6bb092f0492a11a3e879988d53ecdf
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-04-25 13:16:33 +0300

    Code arranging

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -13,19 +13,19 @@ function Skills(){
       <div className='reveal-skills skill_reveal'>
          <article className='skills_1'>
           <p className='skill-info'>Web Developer || Designer.</p>
-         <button className='revealing_time'><a className="skills_sets" href="https://monjotronicsolutions.co.ke/" target="_blank" rel='noreferrer noopener'><span>Check Out</span><span></span></a></button>
+         <button className='revealing_time button_reveal_monjo'><a className="skills_sets" href="https://monjotronicsolutions.co.ke/" target="_blank" rel='noreferrer noopener'><span>Check Out</span><span></span></a></button>
         </article>
         <article className='skills_2'>
           <p className='skill-info'>Working with APIs.</p>
-           <button className='revealing_time'><span>Check Out</span><span></span></button>
+           <button className='revealing_time button_reveal_country-flags'><span>Check Out</span><span></span></button>
         </article>
         <article className='skills_3'>
           <p className='skill-info'>Fullstack Developer.</p>
-           <button className='revealing_time'><span>Check Out</span><span></span></button>
+           <button className='revealing_time button_reveal_issue-tracker'><span>Check Out</span><span></span></button>
         </article>
          <article className='skills_4'>
           <p className='skill-info'>SEO Practioner.</p>
-           <button className='revealing_time'><span>Check Out</span><span></span></button>
+           <button className='revealing_time button_reveal_practitioner'><span>Check Out</span><span></span></button>
         </article>
       </div>
     </motion.div>
